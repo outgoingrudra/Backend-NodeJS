57d111167760f951ac0afc9684ebe1d4e168e1f2
Rudra Verma <rudraverma781@gmail.com>
2025-09-06 08:51:42 +0530

    work

---
+++ Practice-4/timer.js
@@ -0,0 +1,7 @@
+console.log("Start cooking 🍳");
+
+setTimeout(() => {
+  console.log("Pizza is ready after 3 seconds 🍕");
+}, 3000); // waits 3 seconds, then runs this function
+
+console.log("Meanwhile, setting the table 🍽️");
