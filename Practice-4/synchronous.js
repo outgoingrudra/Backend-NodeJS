af6b940d800fb88ae32e971fa0709cbbf606a321
Rudra Verma <rudraverma781@gmail.com>
2025-09-06 08:50:44 +0530

    work

---
+++ Practice-4/synchronous.js
@@ -0,0 +1,13 @@
+console.log("Start making breakfast");
+
+// Boiling water
+function boilWater() {
+  for (let i = 0; i < 3; i++) {
+    console.log(`Boiling... ${i + 1} sec`);
+  }
+  console.log("Water boiled ✅");
+}
+
+boilWater();
+console.log("Make tea with boiled water ☕");
+console.log("Breakfast ready 🍞🥪");
